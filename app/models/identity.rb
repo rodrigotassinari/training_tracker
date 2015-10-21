224c74aae1a6c1c3020d31a6e172812a74384652
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2015-10-21 17:08:47 -0200

    Tests for identity model

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,3 +1,9 @@
 class Identity < ActiveRecord::Base
   belongs_to :user
+
+  validates :user, presence: true
+  validates :provider, presence: true,
+    inclusion: {in: %w(strava)}, uniqueness: {scope: [:user_id]}
+  validates :uid, presence: true, uniqueness: {scope: [:provider]}
+
 end
--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Identity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  context 'validations' do
+    subject { FactoryGirl.build(:identity) }
+    it { is_expected.to validate_presence_of(:user) }
+    it { is_expected.to validate_presence_of(:provider) }
+    it { is_expected.to validate_presence_of(:uid) }
+    it { is_expected.to validate_inclusion_of(:provider).
+          in_array(%w(strava)) }
+    it { is_expected.to validate_uniqueness_of(:provider).scoped_to(:user_id) }
+    it { is_expected.to validate_uniqueness_of(:uid).scoped_to(:provider) }
+  end
+
 end
