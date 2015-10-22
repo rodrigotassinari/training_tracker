3e21432f5d560d480062ec187e0184842bc7f656
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2015-10-21 22:15:25 -0200

    More tests for profile view

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -8,7 +8,17 @@ RSpec.describe "users/edit.html.erb", type: :view do
   end
   it 'renders a user edit form' do
     render
-    assert_select('form')
-    expect(rendered).to include(user.name)
+    assert_select "form[action=?]", "/user" do
+      assert_select 'input[type=hidden][name=_method][value=put]', 1
+      assert_select 'input[type=text][name=?][value=?]', 'user[name]', user.name
+      assert_select 'input[type=email][name=?][value=?]', 'user[email]', user.email
+      assert_select 'select[name=?]', 'user[locale]' do
+        assert_select 'option[selected][value=?]', user.locale
+      end
+      assert_select 'select[name=?]', 'user[time_zone]' do
+        assert_select 'option[selected][value=?]', user.time_zone
+      end
+    end
+    # expect(rendered).to include(user.name)
   end
 end
