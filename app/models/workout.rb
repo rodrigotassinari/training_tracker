8c6a79b30983668da2ee67f16a80cac6f22ea76d
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2015-10-23 10:55:38 -0200

    Tests for the workout model.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -7,43 +7,43 @@ class Workout < ActiveRecord::Base
   validates :user, presence: true
   validates :kind, presence: true, inclusion: {in: KINDS}
   validates :scheduled_on, presence: true
-  validates :distance, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
   validates :elapsed_time, numericality: {
-    greater_than_or_equal_to: 0, only_integer: true, allow_blank: true}
+    greater_than_or_equal_to: 0, only_integer: true, allow_nil: true}
   validates :moving_time, numericality: {
-    greater_than_or_equal_to: 0, only_integer: true, allow_blank: true}
-  validates :speed_avg, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
-  validates :speed_max, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
+    greater_than_or_equal_to: 0, only_integer: true, allow_nil: true}
   validates :cadence_avg, numericality: {
-    greater_than_or_equal_to: 0, only_integer: true, allow_blank: true}
+    greater_than_or_equal_to: 0, only_integer: true, allow_nil: true}
   validates :cadence_max, numericality: {
-    greater_than_or_equal_to: 0, only_integer: true, allow_blank: true}
+    greater_than_or_equal_to: 0, only_integer: true, allow_nil: true}
   validates :calories, numericality: {
-    greater_than_or_equal_to: 0, only_integer: true, allow_blank: true}
-  validates :elevation_gain, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
-  validates :temperature_avg, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
-  validates :temperature_max, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
-  validates :temperature_min, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
-  validates :watts_avg, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
-  validates :watts_weighted_avg, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
-  validates :watts_max, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
+    greater_than_or_equal_to: 0, only_integer: true, allow_nil: true}
   validates :heart_rate_avg, numericality: {
-    greater_than_or_equal_to: 0, only_integer: true, allow_blank: true}
+    greater_than_or_equal_to: 0, only_integer: true, allow_nil: true}
   validates :heart_rate_max, numericality: {
-    greater_than_or_equal_to: 0, only_integer: true, allow_blank: true}
+    greater_than_or_equal_to: 0, only_integer: true, allow_nil: true}
+  validates :distance, numericality: {
+    greater_than_or_equal_to: 0.0, allow_nil: true}
+  validates :speed_avg, numericality: {
+    greater_than_or_equal_to: 0.0, allow_nil: true}
+  validates :speed_max, numericality: {
+    greater_than_or_equal_to: 0.0, allow_nil: true}
+  validates :elevation_gain, numericality: {
+    greater_than_or_equal_to: 0.0, allow_nil: true}
+  validates :temperature_avg, numericality: {
+    greater_than_or_equal_to: 0.0, allow_nil: true}
+  validates :temperature_max, numericality: {
+    greater_than_or_equal_to: 0.0, allow_nil: true}
+  validates :temperature_min, numericality: {
+    greater_than_or_equal_to: 0.0, allow_nil: true}
+  validates :watts_avg, numericality: {
+    greater_than_or_equal_to: 0.0, allow_nil: true}
+  validates :watts_weighted_avg, numericality: {
+    greater_than_or_equal_to: 0.0, allow_nil: true}
+  validates :watts_max, numericality: {
+    greater_than_or_equal_to: 0.0, allow_nil: true}
   validates :weight_before, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
+    greater_than_or_equal_to: 0.0, allow_nil: true}
   validates :weight_after, numericality: {
-    greater_than_or_equal_to: 0.0, allow_blank: true}
+    greater_than_or_equal_to: 0.0, allow_nil: true}
 
 end
--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Workout, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  context 'validations' do
+    subject { FactoryGirl.build(:workout) }
+    it { is_expected.to validate_presence_of(:user) }
+    it { is_expected.to validate_presence_of(:kind) }
+    it { is_expected.to validate_inclusion_of(:kind).
+          in_array(Workout::KINDS) }
+    it { is_expected.to validate_presence_of(:scheduled_on) }
+    # TODO add .only_integer to these tests after shoulda-matchers is fixed (post v3.0.1)
+    it { is_expected.to validate_numericality_of(:elapsed_time).is_greater_than_or_equal_to(0).allow_nil }
+    it { is_expected.to validate_numericality_of(:moving_time).is_greater_than_or_equal_to(0).allow_nil }
+    it { is_expected.to validate_numericality_of(:cadence_avg).is_greater_than_or_equal_to(0).allow_nil }
+    it { is_expected.to validate_numericality_of(:cadence_max).is_greater_than_or_equal_to(0).allow_nil }
+    it { is_expected.to validate_numericality_of(:calories).is_greater_than_or_equal_to(0).allow_nil }
+    it { is_expected.to validate_numericality_of(:heart_rate_avg).is_greater_than_or_equal_to(0).allow_nil }
+    it { is_expected.to validate_numericality_of(:heart_rate_max).is_greater_than_or_equal_to(0).allow_nil }
+    # /TODO
+    it { is_expected.to validate_numericality_of(:distance).is_greater_than_or_equal_to(0.0).allow_nil }
+    it { is_expected.to validate_numericality_of(:speed_avg).is_greater_than_or_equal_to(0.0).allow_nil }
+    it { is_expected.to validate_numericality_of(:speed_max).is_greater_than_or_equal_to(0.0).allow_nil }
+    it { is_expected.to validate_numericality_of(:elevation_gain).is_greater_than_or_equal_to(0.0).allow_nil }
+    it { is_expected.to validate_numericality_of(:temperature_avg).is_greater_than_or_equal_to(0.0).allow_nil }
+    it { is_expected.to validate_numericality_of(:temperature_max).is_greater_than_or_equal_to(0.0).allow_nil }
+    it { is_expected.to validate_numericality_of(:temperature_min).is_greater_than_or_equal_to(0.0).allow_nil }
+    it { is_expected.to validate_numericality_of(:watts_avg).is_greater_than_or_equal_to(0.0).allow_nil }
+    it { is_expected.to validate_numericality_of(:watts_weighted_avg).is_greater_than_or_equal_to(0.0).allow_nil }
+    it { is_expected.to validate_numericality_of(:watts_max).is_greater_than_or_equal_to(0.0).allow_nil }
+    it { is_expected.to validate_numericality_of(:weight_before).is_greater_than_or_equal_to(0.0).allow_nil }
+    it { is_expected.to validate_numericality_of(:weight_after).is_greater_than_or_equal_to(0.0).allow_nil }
+  end
+
 end
