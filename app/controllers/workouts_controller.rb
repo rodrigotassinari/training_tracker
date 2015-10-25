45801468ca868841ed9396b150dd1851ce1d9721
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2015-10-25 21:40:07 -0200

    Workout edit form.

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,7 @@
 class WorkoutsController < ApplicationController
 
+  before_action :set_workout, only: [:show, :edit, :update, :destroy]
+
   # GET /workouts
   # workouts_path
   def index
@@ -9,7 +11,6 @@ class WorkoutsController < ApplicationController
   # GET /workouts/:id
   # workout_path(:id)
   def show
-    @workout = finder.find(params[:id])
   end
 
   # GET /workouts/new
@@ -20,33 +21,63 @@ class WorkoutsController < ApplicationController
 
   # POST /workouts
   # workouts_path
+  # TODO spec
   def create
-    redirect_to new_workout_path
+    @workout = current_user.workouts.build(workout_params)
+    if @workout.save
+      redirect_to workout_path(@workout),
+        notice: t('.success')
+    else
+      render :new
+    end
   end
 
   # GET /workouts/:id/edit
   # edit_workout_path(:id)
+  # TODO spec
   def edit
-    @workout = finder.find(params[:id])
   end
 
   # PUT /workouts/:id
   # PATCH /workouts/:id
   # workout_path(:id)
+  # TODO spec
   def update
-    @workout = finder.find(params[:id])
+    if @workout.update(workout_params)
+      redirect_to workout_path(@workout),
+        notice: t('.success')
+    else
+      render :edit
+    end
   end
 
   # DELETE /workouts/:id
   # workout_path(:id)
+  # TODO spec
   def destroy
-    @workout = finder.find(params[:id])
+    @workout.destroy
+    redirect_to workouts_path,
+      notice: t('.success')
   end
 
   private
 
   def finder
     @finder ||= WorkoutFinderService.new(current_user)
+  end
+
+  def set_workout
+    @workout = finder.find(params[:id])
+  end
+
+  def workout_params
+    params.require(:workout).
+      permit(:kind, :scheduled_on, :occurred_on, :name, :description,
+        :observations, :coach_observations, :weight_before, :weight_after,
+        :distance, :elapsed_time, :moving_time, :speed_avg, :speed_max,
+        :cadence_avg, :cadence_max, :calories, :elevation_gain,
+        :temperature_avg, :temperature_max, :temperature_min, :watts_avg,
+        :watts_weighted_avg, :watts_max, :heart_rate_avg, :heart_rate_max)
   end
 
 end
