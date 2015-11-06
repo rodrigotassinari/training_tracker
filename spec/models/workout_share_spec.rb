b177f8219643eb07ca70aa2a7e50c374f7dcf250
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2015-11-06 13:36:23 -0200

    Adds more validation tests to workout share

--- spec/models/workout_share_spec.rb
+++ spec/models/workout_share_spec.rb
@@ -7,6 +7,13 @@ describe WorkoutShare do
     subject { described_class.new(workout: workout, emails: 'foo@bar.com') }
     it { is_expected.to validate_presence_of(:workout) }
     it { is_expected.to validate_presence_of(:emails) }
+    it { is_expected.to allow_value('foo@bar.com').for(:emails) }
+    it { is_expected.to allow_value('foo@bar.com, ').for(:emails) }
+    it { is_expected.to allow_value('foo@bar.com, spam@eggs.org').for(:emails) }
+    it { is_expected.to_not allow_value('foo').for(:emails) }
+    it { is_expected.to_not allow_value('foo@').for(:emails) }
+    it { is_expected.to_not allow_value('foo@bar.com, spam').for(:emails) }
+    it { is_expected.to_not allow_value(', foo@bar.com').for(:emails) }
   end
 
 end
