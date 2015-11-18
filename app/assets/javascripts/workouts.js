8d83ba86b3f5d439cea4bc7b5083e3c84393aa31
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2015-11-18 18:32:32 -0200

    Shows quick action links (think shortcuts) on workouts index page.

--- app/assets/javascripts/workouts.js
+++ app/assets/javascripts/workouts.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on('ready page:load', function(event) {
+  // apply non-idempotent transformations to the body
+
+  $('div.workout-summary').on('mouseover', function(){
+    $('div.quick-actions', this).removeClass('hidden');
+  }).on('mouseout', function(){
+    $('div.quick-actions', this).addClass('hidden');
+  });
+
+});
