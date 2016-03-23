cd15cb50970efcc3fe86aa204d96626fb869a6ae
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2016-03-23 12:31:15 -0300

    Fix sucker_punch syntax

--- config/initializers/sucker_punch.rb
+++ config/initializers/sucker_punch.rb
@@ -1,5 +1,9 @@
 # https://github.com/brandonhilkert/sucker_punch#active-job
 # http://guides.rubyonrails.org/active_job_basics.html
+
+# If you want to use Sucker Punch version 2.0.0+ with Rails < 5.0.0, be sure to include the backwards compatibility module:
+require 'sucker_punch/async_syntax'
+
 Rails.application.configure do
   config.active_job.queue_adapter = :sucker_punch
 end
